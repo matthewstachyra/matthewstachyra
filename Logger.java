4817f1dbeae6a6a9d8ed58255aeaf6cae2949b4a
matthewstachyra <92053626+matthewstachyra@users.noreply.github.com>
2021-10-12 19:37:40 -0400

    Delete Logger.java

--- Logger.java
+++
@@ -1,8 +0,0 @@
-public class Logger implements BoardListener {
-  public void onMove(String from, String to, Piece p) {
-	  System.out.println("Moved " + p.toString() + " from " + from + " to " + to);
-  }
-  public void onCapture(Piece attacker, Piece captured) {
-	  System.out.println(attacker.toString() + " captures " + captured.toString());
-  }
-}
