4927e38c7a289ed695b0845cf5aaad422985d3b1
matthewstachyra <92053626+matthewstachyra@users.noreply.github.com>
2021-10-12 19:37:00 -0400

    Delete BoardPrinter.java

--- BoardPrinter.java
+++
@@ -1,7 +0,0 @@
-public class BoardPrinter implements BoardExternalIterator {
-  public void visit(String loc, Piece p) {
-    if (p != null) {
-      System.out.println(loc + "=" + p.toString());
-    }
-  }
-}
